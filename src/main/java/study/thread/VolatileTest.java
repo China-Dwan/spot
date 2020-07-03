0f91c3e3f9be5b8b03621f1d44dd40d76ad4eab3
dangzh <dangzh@ciics.cn>
2020-07-03 18:05:37 +0800

    '添加'

--- src/main/java/study/thread/VolatileTest.java
+++ src/main/java/study/thread/VolatileTest.java
@@ -5,19 +5,28 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * volatile关键字
+ */
 public class VolatileTest {
 
+    /**
+     * 三个特性
+     * 1，保证线程可见性
+     * 2，禁止指令重排序
+     * 3，无法保证原子性
+     */
+
     public static void main(String[] args) {
-//        testVolatile();
-        testVolatile1();
+
     }
 
 
     /**
+     * 保证线程可见性
      * 对于jvm堆内存来说，堆内存创建一个共享对象，当线程使用到这个对象时，就会将对象信息copy到线程自己的工作内存中，这样就导致本线程运行
      * 过程中如果其他线程改变了这个对象信息，当前运行线程是感知不到的，加上volatile就是线程每次使用这个对象时都会从内存中读取最新的值
      */
-    // 保证线程可见性
     boolean b = true;
     //    volatile boolean b = true;
     public static void testVolatile() {
@@ -41,12 +50,12 @@ public class VolatileTest {
 
 
     /**
+     * 禁止指令重排序验证
      * 创建一个对象分为很多部分，了解到的  1分配内存空间  2初始化对象  3将内存空间地址赋值给对应的引用
      * 因为CPu执行时，会自动对代码执行顺序进行优化，执行顺序可能为1-3-2
      * 这样当极端情况出现时，就会A线程进入判断是空，获得锁，开始创建对象，创建对象顺序为1-3-2，B线程同时执行，就会发现对象不是空，直接拿到未初始化的对象，这样就是错误的
      * 而加上volatile后，就保证对象创建的执行顺序，其他代码执行过程不会发生错误
      */
-    //禁止指令重排序验证
     static class SS {
         //        private static SS ss;
         private volatile static SS ss;
@@ -66,7 +75,6 @@ public class VolatileTest {
             return ss;
         }
     }
-
 
 
     // 无法保证原子性
